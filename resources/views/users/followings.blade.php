9fb614b203ee0c5bcb252af74cfb7472991166bd
tomoya.seki <te4090st@gmail.com>
2020-12-05 13:28:30 +0000

    follow, unfollow

--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends ('layouts.app')
 
-@section('content')
+@section ('content')
   <div class="row">
     <aside class="col-sm-4">
       {{-- ユーザ情報 --}}
@@ -9,12 +9,8 @@
     <div class="col-sm-8">
       {{-- タブ --}}
       @include('users.navtabs')
-      @if (Auth::id() == $user->id)
-        {{-- 投稿フォーム --}}
-        @include('microposts.form')
-      @endif
-      {{-- 投稿一覧 --}}
-      @include('microposts.microposts')
+      {{-- ユーザ一覧 --}}
+      @include('users.users')
     </div>
   </div>
 @endsection
